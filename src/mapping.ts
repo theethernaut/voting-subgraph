b3312ba03114d524a7240d27a322223fce7b9107
ajs <palebluedot@gmail.com>
2020-05-08 15:23:52 -0300

    Working subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,80 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { BigInt } from '@graphprotocol/graph-ts'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+// Events.
+import {
+  StartVote as StartVoteEvent,
+  CastVote as CastVoteEvent,
+  ExecuteVote as ExecuteVoteEvent
+} from '../generated/Voting/Voting'
+
+// Contracts.
+import {
+  Voting as VotingContract
+} from '../generated/Voting/Voting'
+
+// Schema.
+import {
+  Vote as VoteEntity,
+  Cast as CastEntity
+} from '../generated/schema'
+
+// StartVote(uint256 indexed voteId, address indexed creator, string metadata)
+export function handleStartVote(event: StartVoteEvent): void {
+  // Create vote entity.
+  let vote = new VoteEntity(event.params.voteId.toHex())
+
+  // Properties extracted from the event.
+  vote.creator = event.params.creator
+  vote.metadata = event.params.metadata
+
+  // Properties extracted from the contract.
+  // let voting = VotingContract.bind(event.address)
+  // vote.startTimestamp =
+  // vote.snapshotBlock =
+  // vote.supportRequiredPct =
+  // vote.minAcceptQuorumSupport =
+  // vote.votingPower =
+  // vote.executionScript =
+
+  // Other properties.
+  vote.executed = false
+  vote.casts = []
+
+  vote.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+// CastVote(uint256 indexed voteId, address indexed voter, bool supports, uint256 stake)
+export function handleCastVote(event: CastVoteEvent): void {
+  // Identify parent vote entity.
+  let voteId = event.params.voteId
+  let vote = VoteEntity.load(voteId.toHex())
+  let numCasts = vote.casts.length
+
+  // Create cast entity.
+  let castId = voteId.toHex() + '-' + numCasts.toString()
+  let cast = new CastEntity(castId)
+
+  // Properties extracted from the event.
+  cast.voteId = voteId.toString()
+  cast.voter = event.params.voter
+  cast.supports = event.params.supports
+  cast.voterStake = event.params.stake.toString()
+  cast.save()
+
+  // Register the cast in the parent vote entity.
+  let casts = vote.casts
+  casts.push(castId)
+  vote.casts = casts
+  vote.save()
+}
+
+// ExecuteVote(uint256 indexed voteId)
+export function handleExecuteVote(event: ExecuteVoteEvent): void {
+  // Identify associated vote entity.
+  let voteId = event.params.voteId
+  let vote = VoteEntity.load(voteId.toHex())
+
+  // Mark it as executed.
+  vote.executed = true
+  vote.save()
 }
